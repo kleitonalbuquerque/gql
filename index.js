6e1227dd7864e0839c2e91f0706c9c080af0b603
kleitonalbuquerque <kleiton2102@gmail.com>
2021-03-28 15:22:34 -0300

    Criando tipos (scalar)

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+  scalar Date
+
+  type Usuario {
+    id: ID!
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   # Pontos de entrada da API
   type Query {
-    ola: String
-    horaAtual: String
+    ola: String!
+    horaAtual: Date!
+    usuarioLogado: Usuario
   }
 `
 
@@ -15,7 +27,18 @@ const resolvers = {
     },
     horaAtual() {
       let time = new Date
-      return time.toString()
+      // return time.toString()
+      return time
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: 'Mariana Futura TI',
+        email: 'mariana@email.com',
+        idade: 1,
+        salario: 10000,
+        vip: true
+      }
     }
   }
 }
