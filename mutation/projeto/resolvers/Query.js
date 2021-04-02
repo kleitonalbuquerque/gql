145ac23515785b48d6f20b86390ce07516385bc8
kleitonalbuquerque <kleiton2102@gmail.com>
2021-04-02 16:24:45 -0300

    Primeira Mutation (Novo Usuário)

--- mutation/projeto/resolvers/Query.js
+++ mutation/projeto/resolvers/Query.js
@@ -1,20 +1,18 @@
-const { usuarios, perfis } = require('../data/db')
+const { usuarios, perfis } = require("../data/db");
 
 module.exports = {
-    usuarios() {
-        return usuarios
-    },
-    usuario(_, { id }) {
-        const sels = usuarios
-            .filter(u => u.id === id)
-        return sels ? sels[0] : null
-    },
-    perfis() {
-        return perfis
-    },
-    perfil(_, { id }) {
-        const sels = perfis
-            .filter(p => p.id === id)
-        return sels ? sels[0] : null 
-    }
-}
+  usuarios() {
+    return usuarios;
+  },
+  usuario(_, { id }) {
+    const sels = usuarios.filter((u) => u.id === id);
+    return sels ? sels[0] : null;
+  },
+  perfis() {
+    return perfis;
+  },
+  perfil(_, { id }) {
+    const sels = perfis.filter((p) => p.id === id);
+    return sels ? sels[0] : null;
+  },
+};
