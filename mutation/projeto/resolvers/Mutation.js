8e44d83708e9d3d5ca587b3e436013cef2b09ee2
kleitonalbuquerque <kleiton2102@gmail.com>
2021-04-02 18:13:12 -0300

    Mutation para Alterar Usuário

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -19,6 +19,7 @@ module.exports = {
     usuarios.push(novo);
     return novo;
   },
+
   excluirUsuario(_, { id }) {
     const i = usuarios.findIndex((u) => u.id === id);
 
@@ -26,5 +27,26 @@ module.exports = {
 
     const excluidos = usuarios.splice(i, 1);
     return excluidos ? excluidos[0] : null;
+  },
+
+  alterarUsuario(_, args) {
+    const i = usuarios.findIndex((u) => u.id === args.id);
+
+    if (i < 0) return null;
+
+    usuarios[i].nome = args.nome;
+    usuarios[i].email = args.email;
+    if (args.idade) {
+      usuarios[i].idade = args.idade;
+    }
+
+    // const usuario = {
+    //   ...usuarios[i],
+    //   ...args,
+    // };
+
+    // usuarios.splice(i, 1, usuario);
+    // return usuario;
+    return usuarios[i];
   },
 };
