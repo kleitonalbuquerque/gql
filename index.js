507419d84608784a2b5ad1979115e2500f8698ea
kleitonalbuquerque <kleiton2102@gmail.com>
2021-04-02 14:37:08 -0300

    Usando Resolver para Relacionar Tipos

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require("apollo-server");
 
 const perfis = [
   {
     id: 1,
-    nome: 'Comum'
+    nome: "Comum",
   },
   {
     id: 2,
-    nome: 'Administrador'
-  }
-]
+    nome: "Administrador",
+  },
+];
 
 const usuarios = [
   {
     id: 1,
-    nome: 'João Silva',
-    email: 'jsilva@zemail.com',
-    idade: 30
+    nome: "João Silva",
+    email: "jsilva@zemail.com",
+    idade: 30,
+    perfil_id: 1,
   },
   {
     id: 2,
-    nome: 'Luiz Paulo F de Albuquerque',
-    email: 'lpa@zemail.com',
-    idade: 38
+    nome: "Luiz Paulo F de Albuquerque",
+    email: "lpa@zemail.com",
+    idade: 38,
+    perfil_id: 2,
   },
   {
     id: 3,
-    nome: 'Mariana Oliveira de Albuquerque',
-    email: 'marioa@zemail.com',
-    idade: 22
-  }
-]
+    nome: "Mariana Oliveira de Albuquerque",
+    email: "marioa@zemail.com",
+    idade: 22,
+    perfil_id: 1,
+  },
+];
 
 const typeDefs = gql`
   scalar Date
@@ -44,7 +47,7 @@ const typeDefs = gql`
     nome: String!
     preco: Float!
     desconto: Float
-    precoComDesconto: Float    
+    precoComDesconto: Float
   }
 
   type Usuario {
@@ -54,6 +57,7 @@ const typeDefs = gql`
     idade: Int
     salario: Float
     vip: Boolean
+    perfil: Perfil
   }
 
   # Pontos de entrada da API
@@ -68,77 +72,84 @@ const typeDefs = gql`
     perfis: [Perfil]
     perfil(id: Int): Perfil
   }
-`
+`;
 
 const resolvers = {
   Produto: {
     precoComDesconto(produto) {
-      if(produto.desconto) {
-        return produto.preco * (1 - produto.desconto)
+      if (produto.desconto) {
+        return produto.preco * (1 - produto.desconto);
       } else {
-        produto.preco
+        produto.preco;
       }
-    }
+    },
   },
-  Usuario : {
+  Usuario: {
     salario(usuario) {
-      return usuario.salario_real
-    }
+      return usuario.salario_real;
+    },
+    perfil(usuario) {
+      const sels = perfis.filter((p) => p.id === usuario.perfil_id);
+      return sels ? sels[0] : null;
+    },
   },
   Query: {
     ola() {
-      return 'Bom dia!'
+      return "Bom dia!";
     },
     horaAtual() {
-      let time = new Date
+      let time = new Date();
       // return time.toString()
-      return time
+      return time;
     },
     usuarioLogado() {
       return {
         id: 1,
-        nome: 'Mariana Futura TI',
-        email: 'mariana@email.com',
+        nome: "Mariana Futura TI",
+        email: "mariana@email.com",
         idade: 1,
         salario_real: 10000,
-        vip: true
-      }
+        vip: true,
+      };
     },
     produtoEmDestaque() {
       return {
-        nome: 'Notebook',
+        nome: "Notebook",
         preco: 5000,
-        desconto: 0.1
-      }
+        desconto: 0.1,
+      };
     },
     numerosMegaSena() {
-      const crescente = (a, b) => a - b
-      numbers = Array(6).fill(0).map(n => parseInt(Math.random() * 60 + 1)).sort(crescente)
-      console.log(numbers)
-      return numbers
+      const crescente = (a, b) => a - b;
+      numbers = Array(6)
+        .fill(0)
+        .map((n) => parseInt(Math.random() * 60 + 1))
+        .sort(crescente);
+      console.log(numbers);
+      return numbers;
     },
     usuarios() {
-      return usuarios
+      return usuarios;
     },
     usuario(_, { id }) {
-      const selecionados = usuarios.filter(u => u.id === id)
-      return selecionados ? selecionados[0] : null
+      const selecionados = usuarios.filter((u) => u.id === id);
+      return selecionados ? selecionados[0] : null;
     },
     perfis() {
-      return perfis
+      return perfis;
     },
     perfil(_, { id }) {
-      const selecionados = perfis.filter(p => p.id === id)
-      return selecionados ? selecionados[0] : null
-    }
-  }
-}
+      const selecionados = perfis.filter((p) => p.id === id);
+      return selecionados ? selecionados[0] : null;
+    },
+  },
+};
 
 const serer = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
 serer.listen().then(({ url }) => {
-  console.log(`Executando em ${url}`)
-})
+  console.log(`Executando em ${url}`);
+});
