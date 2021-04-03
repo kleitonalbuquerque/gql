c6698942a2d3a1f4ee05a00b15cf66574f9460fd
kleitonalbuquerque <kleiton2102@gmail.com>
2021-04-03 15:14:17 -0300

    Usando Input

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -1,9 +1,20 @@
 const { usuarios, proximoId } = require("../data/db");
 
+function indiceUsuario(filtro) {
+  if (!filtro) return -1;
+  const { id, email } = filtro;
+  if (id) {
+    return usuarios.findIndex((u) => u.id === id);
+  } else if (email) {
+    return usuarios.findIndex((u) => u.email === email);
+  }
+  return -1;
+}
+
 module.exports = {
   // argumentos { nome, email, idade }
-  novoUsuario(_, args) {
-    const emailExistente = usuarios.some((u) => u.email === args.email);
+  novoUsuario(_, { dados }) {
+    const emailExistente = usuarios.some((u) => u.email === dados.email);
 
     if (emailExistente) {
       throw new Error("E-mail já cadastrado!");
@@ -11,7 +22,7 @@ module.exports = {
 
     const novo = {
       id: proximoId(),
-      ...args,
+      ...dados,
       perfil_id: 1,
       status: "ATIVO",
     };
@@ -20,8 +31,8 @@ module.exports = {
     return novo;
   },
 
-  excluirUsuario(_, { id }) {
-    const i = usuarios.findIndex((u) => u.id === id);
+  excluirUsuario(_, { filtro }) {
+    const i = indiceUsuario(filtro);
 
     if (i < 0) return null;
 
